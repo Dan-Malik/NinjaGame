6df47c440803f286ef4a3a23aa43855cea149175
unknown <Danial.Malik1992@gmail.com>
2019-08-29 23:07:26 +0100

    Pre branching commit

--- core/src/com/dantech/escape/LevelScreen.java
+++ core/src/com/dantech/escape/LevelScreen.java
@@ -24,8 +24,8 @@ public class LevelScreen extends ScreenAdapter {
     SpriteBatch sb;
     float screenWidth = Gdx.graphics.getWidth();
     float screenHeight = Gdx.graphics.getHeight();
-    float topMargin = screenHeight/20;
-    float leftMargin = screenWidth/26;
+    float topMargin = screenHeight / 20;
+    float leftMargin = screenWidth / 26;
 
 
     Stage stage;
@@ -33,22 +33,35 @@ public class LevelScreen extends ScreenAdapter {
     Skin skin;
     ///
 
-    private void generateButtonRows(int numberOfLevels){
-        float buttonGap = screenWidth/26;
-        float buttonWidth = screenWidth/13;
-        float buttonHeight = screenHeight/10;
+    private void generateButtonRows(int numberOfLevels) {
+        float buttonGap = screenWidth / 26;
+        float buttonWidth = screenWidth / 13;
+        float buttonHeight = screenHeight / 10;
 
-        double numberOfRows = Math.ceil((double)numberOfLevels/8);
+        double numberOfRows = Math.ceil((double) numberOfLevels / 8);
 
         //Loop through each row
-        for(int i=1;i<=numberOfRows;i++){
+        for (int i = 1; i <= numberOfRows; i++) {
             //Each cell of the row
-            for(int j=1;j<=8;j++){
-                String cellValue = String.valueOf((i-1)*8+j);
-                TextButton button = new TextButton(cellValue,skin,"default");
+            for (int j = 1; j <= 8; j++) {
+                String cellValue = String.valueOf((i - 1) * 8 + j);
+                TextButton button = new TextButton(cellValue, skin, "default");
+
+                //Check if level not unlocked
+                if (true) {
+                    button.setDisabled(true);
+                }
                 button.setHeight(buttonHeight);
                 button.setWidth(buttonWidth);
-                button.setPosition(leftMargin+(j-1)*(buttonWidth+buttonGap), screenHeight-(i* (2*buttonGap) + buttonHeight));
+                button.setPosition(leftMargin + (j - 1) * (buttonWidth + buttonGap), screenHeight - (i * (2 * buttonGap) + buttonHeight));
+//                button.addListener(new ClickListener() {
+//                    @Override
+//                    public void clicked(InputEvent event, float x, float y) {
+//                        ((Game) Gdx.app.getApplicationListener()).setScreen(new PlayScreen());
+//
+//                    }
+//                });
+
                 stage.addActor(button);
             }
         }
@@ -64,17 +77,9 @@ public class LevelScreen extends ScreenAdapter {
         Gdx.input.setInputProcessor(stage);
         skin = new Skin(Gdx.files.internal("skins/uiskin.json"));
 
-        generateButtonRows(24);
+        ///
 
-//        final TextButton level1 = new TextButton("1", skin, "default");
-//        level1.setHeight(Gdx.graphics.getHeight()/10);
-//        level1.setWidth(Gdx.graphics.getWidth()/12);
-//        level1.setPosition(Gdx.graphics.getWidth() /2, Gdx.graphics.getHeight()/2-level1.getHeight()/2);
-//
-//        final TextButton level2 = new TextButton("2", skin, "default");
-//        level2.setHeight(Gdx.graphics.getHeight()/10);
-//        level2.setWidth(Gdx.graphics.getWidth()/12);
-//        level2.setPosition(Gdx.graphics.getWidth() /2, Gdx.graphics.getHeight()/2-level1.getHeight()*2);
+        generateButtonRows(24);
 
         /// BUTTON TEXT TOO SMALL, ESPECIALLY ON PHONE
 
@@ -85,9 +90,7 @@ public class LevelScreen extends ScreenAdapter {
 //
 //            }
 //        });
-//
-//        stage.addActor(level1);
-//        stage.addActor(level2);
+
 
         generateButtonRows(24);
 
@@ -112,10 +115,9 @@ public class LevelScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 
-
         sb.begin();
         sb.draw(menuBackground, 0, 0, Gdx.graphics.getWidth() * 1.2f, Gdx.graphics.getHeight() * 1.2f);
-        sb.draw(logo, (-Gdx.graphics.getWidth() - logo.getWidth()) / 2, -Gdx.graphics.getHeight() / 8*5, logo.getWidth(), logo.getHeight());
+        sb.draw(logo, (-Gdx.graphics.getWidth() - logo.getWidth()) / 2, -Gdx.graphics.getHeight() / 8 * 5, logo.getWidth(), logo.getHeight());
         ///
 
         stage.draw();
